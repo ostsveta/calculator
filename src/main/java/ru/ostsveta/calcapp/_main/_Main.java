2e14b020361be509c3080b24f4ee447ffd971421
Светлана <sv.agfnva@gmail.com>
2024-10-13 14:25:16 +0400

    Исправлены переменные в классе _Main и код в классе Calculator

--- src/main/java/ru/ostsveta/calcapp/_main/_Main.java
+++ src/main/java/ru/ostsveta/calcapp/_main/_Main.java
@@ -15,17 +15,15 @@ public class _Main {
         ResultWriterService.printResult("+", num1, num2, res);
 
         System.out.println("Пожалуйста, введите два числа для подсчета их РАЗНИЦЫ:");
-        Scanner sc1 = new Scanner(System.in);
-        int num3 = sc1.nextInt();
-        int num4 = sc1.nextInt();
-        int res2 = Calculator.subtrakt(num3, num4);
-        ResultWriterService.printResult("-", num3, num4, res2);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        res = Calculator.subtrakt(num1, num2);
+        ResultWriterService.printResult("-", num1, num2, res);
 
         System.out.println("Пожалуйста, введите два числа для подсчета их ПРОИЗВЕДЕНИЯ:");
-        Scanner sc2 = new Scanner(System.in);
-        int num5 = sc2.nextInt();
-        int num6 = sc2.nextInt();
-        int res3 = Calculator.multiply(num5, num6);
-        ResultWriterService.printResult("*", num5, num6, res3);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        res = Calculator.multiply(num1, num2);
+        ResultWriterService.printResult("*", num1, num2, res);
     }
 }
--- src/main/java/ru/ostsveta/calcapp/util/Calculator.java
+++ src/main/java/ru/ostsveta/calcapp/util/Calculator.java
@@ -3,18 +3,15 @@ package ru.ostsveta.calcapp.util;
 public class Calculator {
 
     public static int add(int x, int y) {
-        int res = x + y;
-        return res;
+        return x + y;
     }
 
     public static int subtrakt(int x, int y) {
-        int res = x - y;
-        return res;
+        return x - y;
     }
 
     public static int multiply(int x, int y) {
-        int res = x * y;
-        return res;
+        return x * y;
     }
 }
 
